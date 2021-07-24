e3f290ffe15cf1cdefddb01bbc6ad07546fe7bb5
ThatDude6310 <87897759+ThatDude6310@users.noreply.github.com>
2021-07-24 11:25:12 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box= createSprite(200,200,20,20);
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+  if (keyIsDown(RIGHT_ARROW)){
+    box.x=box.x+3
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    box.x=box.x-3
+  }
+
+}
+
+
+
+
